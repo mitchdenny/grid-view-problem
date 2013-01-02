6bfcc06245efa98226b238c2ef7f0533b1508059
Mitch Denny <mitch@mitchdenny.com>
2013-01-02 16:36:58 +1100

    Added simple domain model.

---
+++ GridViewProblem/DomainModel/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridViewProblem.DomainModel
+{
+    public class Location
+    {
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return Description;
+        }
+    }
+}
---
+++ GridViewProblem/HubPageModel.cs
@@ -0,0 +1,36 @@
+﻿using GridViewProblem.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridViewProblem
+{
+    public class HubPageModel
+    {
+        public HubPageModel()
+        {
+            People = new ObservableCollection<Person>()
+            {
+                new Person() { FirstName = "Montgomery", LastName = "Burns" },
+                new Person() { FirstName = "Lisa", LastName = "Simpson" },
+                new Person() { FirstName = "Homer", LastName = "Simpson" },
+                new Person() { FirstName = "Marg", LastName = "Simpson" },
+                new Person() { FirstName = "Bart", LastName = "Simpson" }
+            };
+
+            Locations = new ObservableCollection<Location>()
+            {
+                new Location() { Description = "Nuclear Power Plant" },
+                new Location() { Description = "Moe's Tavern" },
+                new Location() { Description = "Springfield Elementary" },
+                new Location() { Description = "Shelbyville" }
+            };
+        }
+
+        public ObservableCollection<Person> People { get; set; }
+        public ObservableCollection<Location> Locations { get; set; }
+    }
+}
